34e916440d44b386db39a764f854adb4b8e03235
Mars <861521087@qq.com>
2024-07-18 20:56:34 +0800

    f

--- docs/.vuepress/.temp/pages/index.html.js
+++
@@ -1,3 +0,0 @@
-import comp from "F:/vuepress/vuepress-starter/docs/.vuepress/.temp/pages/index.html.vue"
-const data = JSON.parse("{\"path\":\"/\",\"title\":\"test\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[{\"level\":2,\"title\":\"image\",\"slug\":\"image\",\"link\":\"#image\",\"children\":[]},{\"level\":2,\"title\":\"code\",\"slug\":\"code\",\"link\":\"#code\",\"children\":[]},{\"level\":2,\"title\":\"code import\",\"slug\":\"code-import\",\"link\":\"#code-import\",\"children\":[]}],\"git\":{},\"filePathRelative\":\"README.md\"}")
-export { comp, data }
